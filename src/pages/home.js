3ed82904ef033df83c54a2b5a3e63bde0764c417
Soufi4ne <soufiane.le.breton@efrei.net>
2022-07-31 12:48:52 +0200

    home.js creation test

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,1 +1,17 @@
-// test
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+        <h1>Hi, welcome to the MERN Blog</h1>
+        <p>
+            The goal of this project is to create a full website using the MERN Stack : Mongo, Express, React, Node, PLUS TailwindCSS ; to be honest i was not a big fan of CSS frameworks (i used Bootstrap only one time for very small project), but in the current start-up that i work for[a Web Agency], the leav dev forced me to use Tailwind so that my Front Skillz improves. Anyway, i think im gonna make articles about gaming, tech and anime-manga
+        </p>
+        <p>
+            I will present myself : My name is Soufi4ne, im a junior Web Developer and also a student at Efrei Paris. Being a good Full Stack Developer is the first goal i want to acheive, and maybe after that i will specialize in something (?), Honestly i dont know what is the most excited part between Front End and Back End... I like both of them ! There is also DevOps that i like a lot and everything related to Cloud Computing ;)
+        </p>
+    </div>
+  )
+}
+
+export default home
