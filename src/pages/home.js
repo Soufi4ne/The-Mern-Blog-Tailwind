112391933244606b33eed31decc1e2ec10ef5667
Soufi4ne <soufiane.le.breton@efrei.net>
2022-07-31 15:46:46 +0200

    basic home tailwind  style

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div>
-        <h1>Hi, welcome to the MERN Blog</h1>
-        <p>
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray600'>Hi, welcome to the MERN Blog</h1>
+        <p className="mx-auto leading-relaxed text-base mb-4">
             The goal of this project is to create a full website using the MERN Stack : Mongo, Express, React, Node, PLUS TailwindCSS ; to be honest i was not a big fan of CSS frameworks (i used Bootstrap only one time for very small project), but in the current start-up that i work for[a Web Agency], the leav dev forced me to use Tailwind so that my Front Skillz improves. Anyway, i think im gonna make articles about gaming, tech and anime-manga
         </p>
-        <p>
+        <p className="mx-auto leading-relaxed text-base mb-4">
             I will present myself : My name is Soufi4ne, im a junior Web Developer and also a student at Efrei Paris. Being a good Full Stack Developer is the first goal i want to acheive, and maybe after that i will specialize in something (?), Honestly i dont know what is the most excited part between Front End and Back End... I like both of them ! There is also DevOps that i like a lot and everything related to Cloud Computing ;)
+        </p>
+        <p className="mx-auto leading-relaxed text-base mb-4">
+            One of my favorite hobbie is Designing things, i love UX Design using tools like Figma or AdobeXD. I do wireframes, mockup, prototypes and more. If you want to see my creations, i will upload everything in my portfolio (if i manage to finish it one day lol)
         </p>
     </div>
   )
