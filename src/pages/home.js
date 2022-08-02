8e8b34726960f5e90f71bfa6d93c7ccfce75aeb6
Soufi4ne <soufiane.le.breton@efrei.net>
2022-08-02 22:48:18 +0200

    few added things

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const home = () => {
+const Home = () => {
   return (
     <div className='mb-20'>
-        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray600'>Hi, Welcome to the MERN Blog</h1>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-600'>Hi, Welcome to the MERN Blog</h1>
         <p className="mx-auto leading-relaxed text-base mb-4">
-            The goal of this project is to create a full website using the MERN Stack : Mongo, Express, React, Node, PLUS TailwindCSS ; to be honest i was not a big fan of CSS frameworks (i used Bootstrap only one time for very small project), but in the current start-up that i work for[a Web Agency], the leav dev forced me to use Tailwind so that my Front Skillz improves. Anyway, i think im gonna make articles about gaming, tech and anime-manga
+            The goal of this project is to create a full website using the MERN Stack : Mongo, Express, React, Node, PLUS TailwindCSS ; to be honest i was not a big fan of CSS frameworks (i used Bootstrap only one time for very small project), but in the current start-up that im working for [Its a Web Agency], the leav dev forced me to use Tailwind so that my Front Skillz improves. Anyway, i think im gonna make articles about gaming, tech and anime-manga
         </p>
         <p className="mx-auto leading-relaxed text-base mb-4">
             I will present myself : My name is Soufi4ne, im a junior Web Developer and also a student at Efrei Paris. Being a good Full Stack Developer is the first goal i want to acheive, and maybe after that i will specialize in something (?), Honestly i dont know what is the most excited part between Front End and Back End... I like both of them ! There is also DevOps that i like a lot and everything related to Cloud Computing ;)
@@ -17,4 +17,4 @@ const home = () => {
   )
 }
 
-export default home
+export default Home
