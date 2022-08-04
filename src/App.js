9cece2bc8be50026261e9665fcf31d7d9763117e
Soufi4ne <soufiane.le.breton@efrei.net>
2022-08-04 20:23:21 +0200

    some adjustements PLUS imports in app js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import ArticlesList from "./pages/ArticlesList";
+import Article from "./pages/Article";
 
 function App() {
   return (
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,13 +5,14 @@ const Home = () => {
     <div className='mb-20'>
         <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-600'>Hi, Welcome to the MERN Blog</h1>
         <p className="mx-auto leading-relaxed text-base mb-4">
-            The goal of this project is to create a full website using the MERN Stack : Mongo, Express, React, Node, PLUS TailwindCSS ; to be honest i was not a big fan of CSS frameworks (i used Bootstrap only one time for very small project), but in the current start-up that im working for [Its a Web Agency], the leav dev forced me to use Tailwind so that my Front Skillz improves. Anyway, i think im gonna make articles about gaming, tech and anime-manga
+        Thank you for reading MERN Blog, a news and opinion site about tech and things serious developers care about. We're here to inform you and, sometimes, entertain you. We also often write about pop culture topics like anime, TV, and video games: if it's gud, interesting, or inspires us, we want to tell you about it.
         </p>
         <p className="mx-auto leading-relaxed text-base mb-4">
-            I will present myself : My name is Soufi4ne, im a junior Web Developer and also a student at Efrei Paris. Being a good Full Stack Developer is the first goal i want to acheive, and maybe after that i will specialize in something (?), Honestly i dont know what is the most excited part between Front End and Back End... I like both of them ! There is also DevOps that i like a lot and everything related to Cloud Computing ;)
+        We aim to be an inclusive site for developers of any ethnicity, gender or sexual orientation. We expect our writers and commenters to treat those they write about as they would if they met them in person. For more on what that means, on the values we embrace and on what lines we expect writers and commenters not to cross, please read this. You might also be interested in G/O Media's Editorial Code, which you can read here.
         </p>
-        <p className="mx-auto leading-relaxed text-base mb-4">
-            One of my favorite hobbie is Designing things, i love UX Design using tools like Figma or AdobeXD. I do wireframes, mockup, prototypes and more. If you want to see my creations, i will upload everything in my portfolio (if i manage to finish it one day lol)
+        <p className="mx-auto leading-relaxed font-bold text-base mb-4">
+        We crave news. We want scoops! So, please, tip your editors by emailing: tips@mernblog.com<br/>
+        <span className="font-light">(This whole text is from Kotaku/about page)</span>
         </p>
     </div>
   )
