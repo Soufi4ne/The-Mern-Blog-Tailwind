58484e6bfbe650a19c67543207b0543138e5f850
Soufi4ne <soufiane.le.breton@efrei.net>
2022-08-17 18:07:17 +0200

    article content : fixing the adobe thumbnail

---
+++ src/pages/article-content.js
@@ -0,0 +1,34 @@
+const articles = [
+  {
+    name: "learn-figma",
+    title: "Learning the basic of Figma",
+    thumbnail: "/images/figma-ar21.png",
+    content: [
+      `Figma is a powerful design tool that helps you to create anything: websites, applications, logos, and much more.`,
+      `By learning to use Figma, you'll take your first steps into User Interface Design and and User Experience Design. These skills are essential for building a great portfolio for yourself and potentially for your own company.`,
+      `Figma is simple to start using, but there's a lot to learn to use it to its full potential. With the basics of shapes, images, and text, you can easily get started prototyping a website, designing a mobile application, and much more.`,
+    ],
+  },
+  {
+    name: "learn-adobexd",
+    title: "Learning AdobeXD for noobs",
+    thumbnail: "/images/adobexd21.png",
+    content: [
+      `The Adobe software company has offered over time exceptional design software such as Photoshop, Illustrator, InDesign, and many others. Adobe XD is another excellent platform that you can use, and these Adobe XD tutorials will show you exactly how.`,
+      `Adobe XD or Adobe Experience Design CC is a similar platform to Sketch, thus allowing you to build various prototypes for web design or applications. Adobe XD has growth in popularity over time and it is being used more and more by designers and web developers.`,
+      `Its available on Mac and Windows and you can use it to build stunning wireframes, interactive prototypes, and other professional designs. Being a new platform, it comes with a lot of tutorials and freebies that you can use to experience with its features. The purpose of this article is just that: it gathers 20 Best Adobe Xd Tutorials!`,
+    ],
+  },
+  {
+    name: "learn-tailwindCSS",
+    title: "Learning TailwindCSS to become the best Front End Developer!",
+    thumbnail: "/images/tailwindimage.jpg",
+    content: [
+      `This is a beginner-friendly guide to using Tailwind CSS, a relatively new but immensely popular and likely game-changing CSS framework. Tailwind adopts a utility-first approach which makes it easy to customize and even easier to maintain as a projects CSS grows in size.`,
+      `CSS itself is difficult to manage in large projects and this is further exacerbated by traditional CSS frameworks, like Bootstrap, Foundation, and others which come with a long library of predefined components for common items like buttons, nav bars, footers, and so on. In short, CSS quickly becomes a mess: hard to write, hard to reason about, and even harder to update.`,
+      `Tailwind solves many of these challenges by taking a lower-level approach: providing CSS helper classes that can be used to create and customize any design. Its best to see this in action though by giving Tailwind a spin yourself.`,
+    ],
+  },
+];
+
+export default articles
