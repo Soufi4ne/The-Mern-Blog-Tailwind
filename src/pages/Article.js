3d3ad72b8805c747367b36103fb0b4ecaf900433
Soufi4ne <soufiane.le.breton@efrei.net>
2022-08-17 17:03:37 +0200

    bugfix, transforming articleContent.map into article.content.map

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -12,7 +12,7 @@ const Article = () => {
       <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-600">
         Free Article about {article.title}
       </h1>
-      {articleContent.map((paragraph, index) => (
+      {article.content.map((paragraph, index) => (
         <p className="mx-auto leading-relaxed text-base mb-4" key={index}>
           {paragraph}
         </p>
