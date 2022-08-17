2a52147e18756c57068b97c8da61c9882bbd6776
Soufi4ne <soufiane.le.breton@efrei.net>
2022-08-17 18:04:44 +0200

    creating and liking the articles list

---
+++ src/pages/ArticlesList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import articleContent from "./article-content";
+
+const ArticlesList = () => {
+  return (
+    <div>
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-600">
+        Articles
+      </h1>
+      <div className="container py-4 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          {articleContent.map((article, index) => (
+            <div key={index} className="p-4 md:w-1/2">
+              <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <Link to={`/article/${article.name}`}>
+                  <img className="lg-:h-48 md:h-36 w-full object-cover object-center" src={article.thumbnail} alt='blog'/>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ArticlesList;
