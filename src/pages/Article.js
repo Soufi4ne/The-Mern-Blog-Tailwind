12d7b22fe40b525bc8946852f9b43bd5d3e1d8c1
Soufi4ne <soufiane.le.breton@efrei.net>
2022-08-17 17:22:26 +0200

    condition if article does not exist

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -7,10 +7,11 @@ const Article = () => {
   const article = articleContent.find(function take(article) {
     return article.name === name;
   });
+  if (!article) return <h1>We do not have uploaded this article yet</h1>
   return (
     <div>
       <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-600">
-        Free Article about {article.title}
+        Article about {article.title}
       </h1>
       {article.content.map((paragraph, index) => (
         <p className="mx-auto leading-relaxed text-base mb-4" key={index}>
