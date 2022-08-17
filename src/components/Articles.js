d20accf63a909518e6451444d84c0c80586303f9
Soufi4ne <soufiane.le.breton@efrei.net>
2022-08-17 20:47:33 +0200

    bugfix props

---
+++ src/components/Articles.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+//props articles
+const Articles = ({articles}) => {
+  return (
+    <div>
+      {articles.map((article, index) => (
+        <div key={index} className="p-4 md:w-1/2">
+          <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+            <Link to={`/article/${article.name}`}>
+              <img
+                className="lg-:h-48 md:h-36 w-full object-cover object-center"
+                src={article.thumbnail}
+                alt="blog"
+              />
+            </Link>
+
+            <div className="p-6">
+              <Link key={index} to={`/article/${article.name}`}>
+                <h3 className="text-lg font-medium text-gray-900 mb-3">
+                  {article.title}
+                </h3>
+              </Link>
+              <p className="leading-relaxed mb-3">
+                {article.content[0].substring(0, 150)}...
+              </p>
+              {/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring */}
+              <div className="flex item-center flex-wrap">
+                <Link
+                  className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
+                  to={`/article/${article.name}`}
+                >
+                  Read more...
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Articles;
