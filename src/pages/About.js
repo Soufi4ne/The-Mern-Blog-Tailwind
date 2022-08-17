caad07211abe79f388471435763f9fe319f81e79
Soufi4ne <soufiane.le.breton@efrei.net>
2022-08-17 18:05:33 +0200

    about js

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='mb-20'>
+    <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-600'>About</h1>
+    <p className="mx-auto leading-relaxed text-base mb-4">
+        The goal of this project is to create a full website using the MERN Stack : Mongo, Express, React, Node, PLUS TailwindCSS ; to be honest i was not a big fan of CSS frameworks (i used Bootstrap only one time for very small project), but in the current start-up that im working for [Its a Web Agency], the leav dev forced me to use Tailwind so that my Front Skillz improves. Anyway, i think im gonna make articles about gaming, tech and anime-manga
+    </p>
+    <p className="mx-auto leading-relaxed text-base mb-4">
+        I will present myself : My name is Soufi4ne, im a junior Web Developer and also a student at Efrei Paris. Being a good Full Stack Developer is the first goal i want to acheive, and maybe after that i will specialize in something (?), Honestly i dont know what is the most excited part between Front End and Back End... I like both of them ! There is also DevOps that i like a lot and everything related to Cloud Computing ;)
+    </p>
+    <p className="mx-auto leading-relaxed text-base mb-4">
+        One of my favorite hobbie is Designing things, i love UX Design using tools like Figma or AdobeXD. I do wireframes, mockup, prototypes and more. If you want to see my creations, i will upload everything in my portfolio (if i manage to finish it one day lol)
+    </p>
+</div>
+  )
+}
+
+export default About
