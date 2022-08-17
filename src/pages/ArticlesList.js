d4660cd7da9463fee8dc5a890474181e8d36d310
Soufi4ne <soufiane.le.breton@efrei.net>
2022-08-17 20:36:29 +0200

    props articles creation

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import articleContent from "./article-content";
 
+//components
+import Articles from '../components/Articles'
+
 const ArticlesList = () => {
   return (
     <div>
@@ -10,24 +13,7 @@ const ArticlesList = () => {
       </h1>
       <div className="container py-4 mx-auto">
         <div className="flex flex-wrap -m-4">
-          {articleContent.map((article, index) => (
-            <div key={index} className="p-4 md:w-1/2">
-              <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                <Link to={`/article/${article.name}`}>
-                  <img className="lg-:h-48 md:h-36 w-full object-cover object-center" src={article.thumbnail} alt='blog'/>
-                </Link>
-
-                <div className="p-6">
-                  <Link key={index} to={`/article/${article.name}`}><h3 className="text-lg font-medium text-gray-900 mb-3">{article.title}</h3></Link>
-                  <p className="leading-relaxed mb-3">{article.content[0].substring(0,150)}...</p>
-                  {/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring */}
-                  <div className="flex item-center flex-wrap">
-                    <Link className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0" to={`/article/${article.name}`}>Read more...</Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+          <Articles/>
         </div>
       </div>
     </div>
