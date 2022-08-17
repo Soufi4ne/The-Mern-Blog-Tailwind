b9c25f83a9318311fde2434cd47220a1cc63b076
Soufi4ne <soufiane.le.breton@efrei.net>
2022-08-17 16:51:26 +0200

    articleContent dot map

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,15 +1,24 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import articleContent from './article-content'
+import React from "react";
+import { useParams } from "react-router-dom";
+import articleContent from "./article-content";
 
 const Article = () => {
-  const {name} = useParams();
-  const article = articleContent.find(function take (article){ return article.name === name})
+  const { name } = useParams();
+  const article = articleContent.find(function take(article) {
+    return article.name === name;
+  });
   return (
-    <div>       
-       <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-600'>Free Article about {article.title}</h1>
+    <div>
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-600">
+        Free Article about {article.title}
+      </h1>
+      {articleContent.map((paragraph, index) => (
+        <p className="mx-auto leading-relaxed text-base mb-4" key={index}>
+          {paragraph}
+        </p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Article
+export default Article;
